bacb83cc08a9db1b20a834e05ba12d18221feeed
Marco <mrojasm2003@gmail.com>
2024-01-20 10:12:59 +0300

    bilet 4

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, session, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import MetaData, create_engine, text
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
-from os import getenv, environ
+from os import getenv
 from config import CONFIG
-
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 app = Flask(__name__)
 
@@ -33,18 +33,92 @@ with create_engine(app.config['MYSQL_ENGINE_URI']).connect() as connection:
 
 migrate = Migrate(app, db)
 
-from models import create_models
+from models import *
 create_models(app)
 
-from models import *
+def init_login_manager(app):
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message = 'Для доступа к данной странице необходимо пройти процедуру аутентификации.'
+    login_manager.login_message_category = 'warning'
+    login_manager.user_loader(load_user)
 
+init_login_manager(app)
 
+# Главная страница и просмотр события
 @app.route('/')
 def index():
-    test1 = Tests(test='test1')
-    db.session.add(test1)
+    information = {}
+    if request.method == 'POST':
+        year = request.form.get('year')
+        information['year'] = year
+        events = get_event_year(year)
+        return render_template('index.html', events=events, information=information)
+    return render_template('index.html', events=None, information=information)
+
+# Количество сотрудников 
+@app.route('/count_employees')
+@login_required
+def employees():
+    count_employees = count_not_fired()
+    return render_template('employees.html', count_employees=count_employees)
+
+# Авторизация
+def load_user(user_id):
+    user = db.session.execute(db.select(Workers).filter_by(id=user_id)).scalar()
+    return user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    information = {}
+    if request.method == 'POST':
+        login = request.form.get('loginInput')
+        password = request.form.get('passwordInput')
+        information["login"] = login
+        information["password"] = password
+        remember_me = request.form.get('remember_me') == 'on'
+        if login and password:
+            user = db.session.execute(db.select(Workers).filter_by(login=login)).scalar()
+            if user and user.check_password(password):
+                login_user(user, remember=remember_me)
+                flash('Вы успешно аутентифицированы.', 'success')
+                next = request.args.get('next')
+                return redirect(next or url_for('index'))
+        flash('Введены неверные логин и/или пароль.', 'danger')
+    return render_template('login.html', information=information)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+@app.route('/add_data')
+def add_data():
+    
+    user1 = Workers(name='Андрей',login='aobugin',status='Работает')
+    user1.set_password('qwerty1234')
+    db.session.add(user1)
+
+    user2 = Workers(name='Дмитрий',login='dmrukole',status='Уволен')
+    user2.set_password('aebc19kl')
+    db.session.add(user2)
+
+    user3 = Workers(name='Денис',login='dbshafran',status='Работает')
+    user3.set_password('qcvk13091')
+    db.session.add(user3)
+
+    event1 = Events(year=2018, date='15 февраля', desc='Рождение 10000 жителя города')
+    db.session.add(event1)
+
+    event1 = Events(year=1951, date='1 июня', desc='Открытие сталепрокатного завода')
+    db.session.add(event1)
+
+    event1 = Events(year=1812, date='30 октября', desc='Освобождение от войск Наполеона')
+    db.session.add(event1)
+
+    event1 = Events(year=1789, date='1 октября', desc='Первое упоминание о городе')
+    db.session.add(event1)
+
     db.session.commit()
-    
-    
-    text = Tests.query.filter(Tests.id == 1).one()
-    return render_template('index.html', text=text)
+    return redirect(url_for("index"))
